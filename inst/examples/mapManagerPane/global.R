ca143e5ac720e24336f41e730c16a5dd885b45ea
Jeffrey Hanson <jeffrey.hanson@uqconnect.edu.au>
2021-06-01 08:12:02 +1200

    add legend to test app to check alignment

--- inst/examples/mapManagerPane/global.R
+++ inst/examples/mapManagerPane/global.R
@@ -8,21 +8,27 @@ library(locationmisc)
 set.seed(500)
 
 # create items for the map manager
-## create dataset object
-d <- new_dataset(
+## create dataset objects
+d1 <- new_dataset(
   source = tempfile(), total = 14, units = "ha",
   legend = new_categorical_legend(
     c(1e-5, 567, 2938239, 1e7, 1e8),
     c("#e41a1c", "#377eb8", "#4daf4a", "#984ea3", "#ff7f00", "#a6cee3")))
 
-## create weight object
-w <- new_weight("Human footprint index", d)
+d2 <- new_dataset(
+  source = tempfile(), total = 14, units = "ha",
+  legend = new_continuous_legend(
+    0, 100, c("#fee8c8", "#fdbb84", "#e34a33")))
+
+## create weight objects
+w1 <- new_weight("Human footprint index", d1)
+w2 <- new_weight("Fire risk", d2)
 
 ## simulate themes and weights
 ss <- simulate_solution_settings(1, 1, 1)
 
 ## create list of all map layers
-l <- append(append(ss$themes, ss$weights), list(w))
+l <- append(append(ss$themes, ss$weights), list(w1, w2))
 
 # create a map manager object
 mm <- new_map_manager(l)
