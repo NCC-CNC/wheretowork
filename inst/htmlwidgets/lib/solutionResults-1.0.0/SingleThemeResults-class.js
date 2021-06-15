de2ca1f631f1f741b95d80ea7f920def01afce14
Jeffrey Hanson <jeffrey.hanson@uqconnect.edu.au>
2021-06-16 09:27:17 +1200

    add apex charts, #55

--- inst/htmlwidgets/lib/solutionResults-1.0.0/SingleThemeResults-class.js
+++ inst/htmlwidgets/lib/solutionResults-1.0.0/SingleThemeResults-class.js
@@ -26,12 +26,72 @@ class SingleThemeResults {
         .content,
       true);
 
-    // TODO: add code to dipslay results for the object
-    // The current code just inserts a TODO into the DOM
-    const l = document.createElement("label");
-    l.innerText = "TODO";
-    this.el.appendChild(l);
+    // create HTML element for graph
+    const graph_el = document.createElement("div");
 
+    // create parameters for graph
+    const options = {
+      series: [feature_solution_held],
+      chart: {
+        type: "radialBar",
+        offsetY: -20,
+        sparkline: {
+          enabled: true
+        }
+      },
+      plotOptions: {
+        radialBar: {
+          startAngle: -90,
+          endAngle: 90,
+          track: {
+            background: "#e7e7e7",
+            strokeWidth: "97%",
+            margin: 5, // margin is in pixels
+            dropShadow: {
+              enabled: true,
+              top: 2,
+              left: 0,
+              color: "#999",
+              opacity: 1,
+              blur: 2
+            }
+          },
+          dataLabels: {
+            name: {
+              show: false
+            },
+            value: {
+              offsetY: -2,
+              fontSize: "22px"
+            }
+          }
+        }
+      },
+      grid: {
+        padding: {
+          top: -10
+        }
+      },
+      fill: {
+        type: "gradient",
+        gradient: {
+          shade: "light",
+          shadeIntensity: 0.4,
+          inverseColors: false,
+          opacityFrom: 1,
+          opacityTo: 1,
+          stops: [0, 50, 53, 91]
+        },
+      },
+      labels: [feature_name],
+      };
+
+    // initialize graph
+    const graph = new ApexCharts(graph_el, options);
+    graph.render();
+
+    // add graph to element
+    this.el.appendChild(graph_el);
   }
 
   /* render method */
