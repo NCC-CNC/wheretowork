b35fd1aec941e7a2a1b52c040c0a360b8abc4173
Jeffrey Hanson <jeffrey.hanson@uqconnect.edu.au>
2021-07-05 11:38:24 +1200

    add screenshot button to leaflet_map

--- R/leaflet_map.R
+++ R/leaflet_map.R
@@ -18,31 +18,60 @@ leaflet_map <- function() {
     "function(btn, map) {",
     "Shiny.setInputValue(\"hide_button\", Math.random());",
     "}")
+  print_js <- paste0(
+    "function(btn, map) {",
+    "Shiny.setInputValue(\"print_button\", Math.random());",
+    "}")
   # create map
   map <-
+    ## initialize leaflet map
     leaflet::leaflet() %>%
+    ## add basemap
     leaflet::addProviderTiles(
       leaflet::providers$Esri.WorldImagery) %>%
+    ## specify default view window
     leaflet::flyToBounds(
       -165, -30, 165, 60) %>%
-   leaflet::addEasyButton(
+    ## add home button
+    leaflet::addEasyButton(
       leaflet::easyButton(
+        title = "Zoom to home",
         icon = shiny::icon("home"),
         position = "topleft",
         onClick = htmlwidgets::JS(home_js)
       )
     ) %>%
-   leaflet::addEasyButton(
+    ## add hide layers button
+    leaflet::addEasyButton(
       leaflet::easyButton(
+        title = "Hide layers",
         icon = shiny::icon("eye-slash"),
         position = "topleft",
         onClick = htmlwidgets::JS(hide_js)
       )
     ) %>%
+    ## add screenshot button
+    leaflet.extras2::addEasyprint(
+      options = leaflet.extras2::easyprintOptions(
+        exportOnly = TRUE,
+        hidden = TRUE
+      )
+    ) %>%
+    leaflet::addEasyButton(
+      leaflet::easyButton(
+        title = "Take screenshot",
+        icon = shiny::icon("print"),
+        position = "topleft",
+        onClick = htmlwidgets::JS(print_js)
+      )
+    ) %>%
+    ## add history buttons
     leaflet.extras2::addHistory(
       options = leaflet.extras2::historyOptions(position = "topleft")) %>%
+    ## add scale bar
     leaflet::addScaleBar(
       position = "bottomleft") %>%
+    ## add minimap
     leaflet::addMiniMap(
       position = "bottomleft",
       width = 174, height = 150)
