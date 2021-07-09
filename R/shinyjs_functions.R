f513ebd10ff07026535cc83d3804a18e2149ca9f
Jeffrey Hanson <jeffrey.hanson@uqconnect.edu.au>
2021-07-09 12:24:58 +1200

    add js helpers

--- R/shinyjs_functions.R
+++ R/shinyjs_functions.R
@@ -40,3 +40,41 @@ disable_html_element <- function(inputId) {
   )
   invisible(TRUE)
 }
+
+#' Enable a HTML element using a CSS selector
+#'
+#' Enable a HTML element using a CSS selector.
+#'
+#' @param x `character` CSS selector.
+#'
+#' @return Invisible `TRUE`.
+#'
+#' @export
+enable_html_css_selector <- function(x) {
+  shinyjs::runjs(
+    paste0(
+      "document.querySelector('", x, "').classList.remove('disabled');"
+    )
+  )
+  invisible(TRUE)
+}
+
+#' Disable HTML element(s) using a CSS selector
+#'
+#' Disable HTML element(s) using a CSS selector.
+#'
+#' @param x `character` CSS selector.
+#'
+#' @return Invisible `TRUE`.
+#'
+#' @return Invisible `TRUE`.
+#'
+#' @export
+disable_html_css_selector <- function(x) {
+  shinyjs::runjs(
+    paste0(
+      "document.querySelector('", x, "').classList.add('disabled');"
+    )
+  )
+  invisible(TRUE)
+}
