63374d69f3c9b603e384039f2e5b7097603aa97a
Jeffrey Hanson <jeffrey.hanson@uqconnect.edu.au>
2021-10-25 18:32:30 +1300

    fix inconsistent casses in user groups

--- R/app_global.R
+++ R/app_global.R
@@ -48,7 +48,7 @@ app_global <- quote({
   #   then import projects from location specified in golem-config.yml
 
   # set user group
-  user_groups <- Sys.getenv("SHINYPROXY_USERGROUPS")
+  user_groups <- tolower(Sys.getenv("SHINYPROXY_USERGROUPS"))
   if (nchar(user_groups) == 0) {
     user_groups <- "public"
   }
