ed078de422b0ae45cfdb16d8854292761f5f592c
Dan Wismer <daniel.s.wismer@gmail.com>
2022-05-26 16:22:04 -0600

    conditional to check RandomFields in testthat

--- tests/testthat.R
+++ tests/testthat.R
@@ -5,15 +5,10 @@ library(wheretowork)
 # enable parallel testing
 Sys.unsetenv("R_TESTS")
 
-# Set RandomFields options
-
-
-assertthat::assert_that(
-  requireNamespace("RandomFields"), 
-  msg = "please install the \"RandomFields\" package"
-)
-
-RandomFields::RFoptions(cPrintlevel = 0)
+# Set RandomFields options if package exists
+if (requireNamespace("RandomFields")) {
+  RandomFields::RFoptions(cPrintlevel = 0)
+} 
 
 # run tests
 test_check("wheretowork")
