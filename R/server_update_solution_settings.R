44fece3b1f05eb008a49e56af311ade44392b234
Dan Wismer <daniel.s.wismer@gmail.com>
2022-11-16 14:44:00 -0700

    removed warning pop up on excludes
    -  hover icon that displays the same message

--- R/server_update_solution_settings.R
+++ R/server_update_solution_settings.R
@@ -22,50 +22,6 @@ server_update_solution_settings <- quote({
     ## update solution settings object
     app_data$ss$set_setting(input$newSolutionPane_settings)
     
-    ## pop-up for overlap: exclude
-    if ((identical(input$newSolutionPane_settings$type, "exclude")) && input$newSolutionPane_settings$value) {
-      name <- app_data$ss$get_exclude_settings()[app_data$ss$get_exclude_settings()$id == input$newSolutionPane_settings$id,]$name
-      overlap <- app_data$ss$get_exclude_settings()[app_data$ss$get_exclude_settings()$id == input$newSolutionPane_settings$id,]$overlap
-      if (shiny::isTruthy(overlap)) {
-        shinyalert::shinyalert(
-          title = "Warning",
-          text = paste0(name, " overlaps the following includes: ", overlap, ". 
-           *Includes take precedence over excludes in prioritization."),
-          size = "s",
-          closeOnEsc = TRUE,
-          closeOnClickOutside = TRUE,
-          type = "warning",
-          showConfirmButton = TRUE,
-          confirmButtonText = "OK",
-          timer = 0,
-          confirmButtonCol = "#0275d8",
-          animation = TRUE
-        )
-      }
-    }
-    
-    ## pop-up for overlap: include
-    if ((identical(input$newSolutionPane_settings$type, "include")) && input$newSolutionPane_settings$value) {
-      name <- app_data$ss$get_include_settings()[app_data$ss$get_include_settings()$id == input$newSolutionPane_settings$id,]$name
-      overlap <- app_data$ss$get_include_settings()[app_data$ss$get_include_settings()$id == input$newSolutionPane_settings$id,]$overlap
-      if (shiny::isTruthy(overlap)) {
-        shinyalert::shinyalert(
-          title = "Warning",
-          text = paste0(name, " overlaps the following excludes: ", overlap, ". 
-           *Includes take precedence over excludes in prioritization."),
-          size = "s",
-          closeOnEsc = TRUE,
-          closeOnClickOutside = TRUE,
-          type = "warning",
-          showConfirmButton = TRUE,
-          confirmButtonText = "OK",
-          timer = 0,
-          confirmButtonCol = "#0275d8",
-          animation = TRUE
-        )
-      }
-    }    
-    
     ## if updating include status,
     ## then update the current amount for each feature within each theme
     if (identical(input$newSolutionPane_settings$type, "include")) {
