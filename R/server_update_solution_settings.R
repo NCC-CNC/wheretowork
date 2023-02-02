e8088d4b07eea38b91e57e198ae106308e032dac
Dan Wismer <daniel.s.wismer@gmail.com>
2023-02-02 15:21:18 -0700

    if fileinput exists, set_user_settings and update_settings. Update UI

--- R/server_update_solution_settings.R
+++ R/server_update_solution_settings.R
@@ -22,6 +22,62 @@ server_update_solution_settings <- quote({
     ## update solution settings object
     app_data$ss$set_setting(input$newSolutionPane_settings)
     
+    ## set user configs on file upload
+   if(input$newSolutionPane_settings$setting == "fileinput") {
+     app_data$ss$set_user_settings()
+     app_data$ss$update_settings()
+    }
+    
+    ## update settings with user uploaded config file
+    if (length(app_data$ss$user_settings) > 0) {
+      ### update the widget - weights
+      lapply(seq_along(app_data$weights), function(x) {
+        updateSolutionSettings(
+          inputId = "newSolutionPane_settings",
+          value = list(
+            id = app_data$ss$weights[[x]]$id,
+            setting = "status",
+            value = app_data$ss$weights[[x]]$status,
+            type = "weight"
+          )
+        )
+        updateSolutionSettings(
+          inputId = "newSolutionPane_settings",
+          value = list(
+            id = app_data$ss$weights[[x]]$id,
+            setting = "factor",
+            value = app_data$ss$weights[[x]]$factor,
+            type = "weight"
+          )
+        )        
+      })      
+      ### update the widget - includes
+      lapply(seq_along(app_data$includes), function(x) {
+        updateSolutionSettings(
+          inputId = "newSolutionPane_settings",
+          value = list(
+            id = app_data$ss$includes[[x]]$id,
+            setting = "status",
+            value = app_data$ss$includes[[x]]$status,
+            type = "include"
+          )
+         )
+       })
+      ### update the widget - excludes
+      lapply(seq_along(app_data$excludes), function(x) {
+        updateSolutionSettings(
+          inputId = "newSolutionPane_settings",
+          value = list(
+            id = app_data$ss$excludes[[x]]$id,
+            setting = "status",
+            value = app_data$ss$excludes[[x]]$status,
+            type = "exclude"
+          )
+        )
+      })      
+      
+     }
+    
     ## if updating include status,
     ## then update the current amount for each feature within each theme
     if (identical(input$newSolutionPane_settings$type, "include")) {
